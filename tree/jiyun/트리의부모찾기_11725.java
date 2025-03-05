60552f87d18048dfcaf2fcdf0ed6e35878c16fc8
Ryujy <bamdal124@gmail.com>
2025-03-05 11:39:26 +0900

    [BOJ] 트리의 부모찾기 / Silver2 / 428ms

--- tree/jiyun/트리의부모찾기_11725.java
+++ tree/jiyun/트리의부모찾기_11725.java
@@ -5,20 +5,23 @@ import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 public class BOJ_11725_트리의부모찾기 {
-    static ArrayList<ArrayList<Integer>> tree = new ArrayList<>();
+    static ArrayList<Integer>[] tree;
     static int[] parents;
     static boolean[] visited;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
         StringTokenizer st;
 
         int n = Integer.parseInt(br.readLine());
         parents = new int[n+1];
         visited = new boolean[n+1];
 
+        tree = new ArrayList[n+1];
+
         for (int i=0; i<n+1; i++){ // arrayList 초기화
-            tree.add(new ArrayList<>());
+            tree[i] = new ArrayList<>();
         }
 
         for (int i=1; i<n; i++){
@@ -26,23 +29,24 @@ public class BOJ_11725_트리의부모찾기 {
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
 
-            tree.get(a).add(b);
-            tree.get(b).add(a);
+            tree[a].add(b);
+            tree[b].add(a);
         }
 
         // 루트 노트 : 1
         dfs(1, 0);
         for (int i=2; i<n+1; i++){
-            System.out.println(parents[i]);
+            sb.append(parents[i]).append('\n');
         }
+        System.out.println(sb);
     }
 
     private static void dfs(int node, int parent){
         parents[node] = parent;
         visited[node] = true;
 
-        for (int child : tree.get(node)){
-            if (child == parent || visited[child]) continue;
+        for (int child : tree[node]){
+            if (visited[child]) continue;
             dfs(child, node);
         }
     }
